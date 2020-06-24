721c23ead7bc1173ab7438d095f0a3b614a0ee0f
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2020-06-24 11:58:22 -0500

    Improve ghc detection

--- .github/scripts/build_pyduckling.sh
+++ .github/scripts/build_pyduckling.sh
@@ -1,8 +1,7 @@
 
-# GHC_ROOT=$(dirname $(dirname $(stack exec which ghc)))
-# GHC_LIB="$GHC_ROOT/lib/ghc-8.6.5/"
-# RTS_PATH="$GHC_LIB/rts"
+pushd .
+cd $HOME
+stack config set resolver ghc-8.6.5
+popd
 
-# export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$RTS_PATH:$(pwd)/ext_lib
-ls $HOME/.stack/programs/x86_64-linux/
 maturin develop
