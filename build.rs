23689424049a005bff827b77dc8601b9d852f3c7
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2020-06-23 20:42:38 -0500

    Update gitignore

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 
 fn main() {
-    println!("cargo:rustc-link-search=native=/home/andfoy/Documentos/Treble/pyduckling/bin/");
+    println!("cargo:rustc-link-search=native=/home/andfoy/Documentos/Treble/pyduckling/lib/");
     println!("cargo:rustc-link-search=native=/home/andfoy/.stack/programs/x86_64-linux/ghc-tinfo6-8.6.5/lib/ghc-8.6.5/rts");
     println!("cargo:rustc-link-lib=dylib=ducklingffi");
     println!("cargo:rustc-link-lib=dylib=HSrts-ghc8.6.5");
