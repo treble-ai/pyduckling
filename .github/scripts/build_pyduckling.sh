8ae12cb61aa89d8865aa2080f382527a2c3be6c9
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2020-06-23 23:24:33 -0500

    Remove hardcoded paths in build.rs

--- .github/scripts/build_pyduckling.sh
+++ .github/scripts/build_pyduckling.sh
@@ -1,7 +1,7 @@
 
-GHC_ROOT=$(dirname $(dirname $(stack exec which ghc)))
-GHC_LIB="$GHC_ROOT/lib/ghc-8.6.5/"
-RTS_PATH="$GHC_LIB/rts"
+# GHC_ROOT=$(dirname $(dirname $(stack exec which ghc)))
+# GHC_LIB="$GHC_ROOT/lib/ghc-8.6.5/"
+# RTS_PATH="$GHC_LIB/rts"
 
-LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$RTS_PATH:$(pwd)/ext_lib
+# export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$RTS_PATH:$(pwd)/ext_lib
 maturin develop
--- build.rs
+++ build.rs
@@ -1,7 +1,12 @@
 
+use std::env;
+
 fn main() {
-    println!("cargo:rustc-link-search=native=/home/andfoy/Documentos/Treble/pyduckling/lib/");
-    println!("cargo:rustc-link-search=native=/home/andfoy/.stack/programs/x86_64-linux/ghc-tinfo6-8.6.5/lib/ghc-8.6.5/rts");
+    let dir_path = env::current_dir().unwrap();
+    let path = dir_path.to_str().unwrap();
+    let home = env::var("HOME").unwrap();
+    println!("cargo:rustc-link-search=native={}/ext_lib/", path);
+    println!("cargo:rustc-link-search=native={}/.stack/programs/x86_64-linux/ghc-tinfo6-8.6.5/lib/ghc-8.6.5/rts", home);
     println!("cargo:rustc-link-lib=dylib=ducklingffi");
     println!("cargo:rustc-link-lib=dylib=HSrts-ghc8.6.5");
     // println!("cargo:rustc-link-lib=static=ducklingffi");
