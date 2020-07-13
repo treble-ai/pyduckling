d39708d92da9238460f6814adaaff260611ad0e2
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2020-07-13 18:22:46 -0500

    Run tests on CI

--- .github/scripts/run_tests.sh
+++ .github/scripts/run_tests.sh
@@ -1,7 +1,6 @@
 
 # Set LD_LIBRARY_PATH in order to load dynamic libraries
 GHC_PATH=$(stack exec -- ghc --print-libdir)
-GHC_VERSION=$(stack exec -- ghc --numeric-version)
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$GHC_PATH/lib/ghc-${GHC_VERSION}/rts:$(pwd)/ext_lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$GHC_PATH/rts:$(pwd)/ext_lib
 
 pytest -x -v --cov=duckling duckling/tests
