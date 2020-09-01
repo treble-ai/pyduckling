7d24ba00b6721ac183fa48f09e11c2bb21a16016
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2020-09-01 15:01:00 -0500

    Debug ls

--- build_wheels.sh
+++ build_wheels.sh
@@ -51,6 +51,7 @@ PYBIN=$(echo $PYBIN)
 GHC_LIB=$(stack exec -- ghc --print-libdir)
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$GHC_LIB/rts:$(pwd)/ext_lib
 # for PYBIN in /opt/python/cp{35,36,37,38,39}*/bin; do
+ls dist
 "${PYBIN}/pip" install -U setuptools wheel setuptools-rust auditwheel
 "${PYBIN}/python" packaging/build_wheels.py
 # done
