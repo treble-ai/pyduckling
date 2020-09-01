0e6a5c7bbe0c6a1c461e0aa95bd99454b1e9ccd7
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2020-09-01 11:50:51 -0500

    Set resolver directly

--- build_wheels.sh
+++ build_wheels.sh
@@ -15,8 +15,8 @@ export PATH="$HOME/.local/bin:$PATH"
 
 # Set stack resolver to 8.6.5
 # sudo chmod 777 $HOME
-TAR_OPTIONS=--no-same-owner stack setup --allow-different-user
-stack config set resolver ghc-8.6.5 --allow-different-user
+stack config set resolver ghc-8.6.5
+# TAR_OPTIONS=--no-same-owner stack setup --allow-different-user
 
 # Compile patchelf and apply 64MB patch
 pushd /root
