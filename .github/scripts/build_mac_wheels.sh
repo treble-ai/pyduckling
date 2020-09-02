ddad5d6e1ced315047c6d5cde60a02dc96514e3b
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2020-09-02 13:09:20 -0500

    Add all HS libraries into DYLD_LIBRARY_PATH

--- .github/scripts/build_mac_wheels.sh
+++ .github/scripts/build_mac_wheels.sh
@@ -13,6 +13,11 @@ popd
 export PATH=/usr/local/miniconda/envs/test/bin:/usr/local/miniconda/condabin:$PATH
 GHC_LIB=$(stack exec -- ghc --print-libdir)
 export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:$GHC_LIB/rts:$(pwd)/ext_lib
+
+for dir in $GHC_LIB/*/; do
+    export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:$dir
+done
+
 # python setup.py bdist_wheel
 python packaging/build_wheels.py
 
