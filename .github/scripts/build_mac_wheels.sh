78bc535e9dd99da611c212a2b0ef996be7324caf
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2020-09-01 23:36:23 -0500

    Prepend conda to PATH

--- .github/scripts/build_mac_wheels.sh
+++ .github/scripts/build_mac_wheels.sh
@@ -7,8 +7,10 @@ pushd $HOME
 stack config set resolver ghc-8.6.5
 popd
 
-which python
-conda activate test
+# conda activate test
+# which python
+# Adjust PATH in macOS because conda is not at front of it
+export PATH=/usr/local/miniconda/envs/test/bin:/usr/local/miniconda/condabin:$PATH
 python setup.py bdist_wheel
 
 if [[ $PYTHON_VERSION == "3.9" ]]; then
