14a52666d464200e44d68c60e09db91677c1f637
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2020-09-02 16:47:07 -0500

    Add Mac test pipeline

--- .github/scripts/run_tests.sh
+++ .github/scripts/run_tests.sh
@@ -1,6 +1,11 @@
 
 # Set LD_LIBRARY_PATH in order to load dynamic libraries
 GHC_PATH=$(stack exec -- ghc --print-libdir)
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$GHC_PATH/rts:$(pwd)/ext_lib
 
+
+if [[ "$(uname)" != Darwin ]]; then
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$GHC_PATH/rts:$(pwd)/ext_lib
+else
+    export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:$GHC_PATH/rts:$(pwd)/ext_lib
+fi
 pytest -x -v --cov=duckling duckling/tests
